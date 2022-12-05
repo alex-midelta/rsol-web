bb7f451bfa55cb964f3239bd84f5183f9ef03fc1
Alex <118339767+alex-midelta@users.noreply.github.com>
2022-12-04 21:01:55 -0700

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,6 +7,14 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIrHt2l51RtNi9p2JN8qETH0XvUnfG2Rw",
+  authDomain: "rsol-web.firebaseapp.com",
+  projectId: "rsol-web",
+  storageBucket: "rsol-web.appspot.com",
+  messagingSenderId: "900319442075",
+  appId: "1:900319442075:web:6ace27cb12f7d0455cc23e"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
