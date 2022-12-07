b69c99aa60a44cbca73853e81218875defe27b9d
Alejandro <a.lizarraga19@info.uas.edu.mx>
2022-12-07 01:24:28 -0700

    catalogo

--- src/components/Catalogo.vue
+++ src/components/Catalogo.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from 'vue';
 import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore';
 import { db } from '@/firebase';
+import ModalVue from '@/components/Modal.vue'
 
 const catalogoCollectionRef = collection(db, 'productos')
 // const catalogoCollectionQuery = query(catalogoCollectionRef, orderBy('modelo', 'asc'), limit(3));
@@ -64,33 +65,32 @@ const newCatalogoContent = ref('');
             </div>
         </form>
 
-        <div class="tile is-ancestor">
-            <div class="tile is-4">
-                <div v-for="producto in catalogo" class="card mb-5"
-                    :class="{ 'has-background-success-light': producto.disponibilidad }">
-                    <div class="card box">
-    
-                        <header class="card-header">
-                            <p class="card-header-title">
-                                {{ producto.modelo }}
-                            </p>
-                            <!-- <button class="card-header-icon" aria-label="more options">
+        <div class="columns is-mobile is-multiline is-centered">
+            <div v-for="producto in catalogo" class="column"
+                :class="{ 'has-background-success-light': producto.disponibilidad }">
+                <div class="card box">
+
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            {{ producto.modelo }}
+                        </p>
+                        <!-- <button class="card-header-icon" aria-label="more options">
                             <span class="icon">
                                 <i class="fas fa-angle-down" aria-hidden="true"></i>
                             </span>
                         </button> -->
-                        </header>
-    
-                        <div class="card-image">
-                            <figure class="image is-128x128">
-                                <img :src="producto.imagen" alt="">
-                            </figure>
-                        </div>
-    
-                        <div class="card-content">
-                            <div class="columns is-mobile is-vcentered">
-    
-                                <!-- <div class="column is-one-quarter has-text-left">
+                    </header>
+
+                    <div class="card-image">
+                        <figure class="image is-128x128">
+                            <img :src="producto.imagen" alt="">
+                        </figure>
+                    </div>
+
+                    <div class="card-content">
+                        <div class="columns is-mobile is-vcentered">
+
+                            <!-- <div class="column is-one-quarter has-text-left">
     
                                 <div class="row">
                                     <strong>Marca:</strong>
@@ -100,12 +100,13 @@ const newCatalogoContent = ref('');
                                     <p>{{ producto.manual }}</p>
                                 </div>
                             </div> -->
-    
-                                <div class="has-text-left">
-                                    {{ producto.descripcion }}
-                                </div>
-    
-                                <!-- <div class="column">
+
+                            <div class="has-text-left">
+                                <!-- {{ producto.descripcion }} -->
+                                {{ producto.precio }}
+                            </div>
+
+                            <!-- <div class="column">
                                 <button class="button"
                                     :class="producto.disponibilidad ? 'is-success' : 'is-light'">
                                     &check;
@@ -114,82 +115,20 @@ const newCatalogoContent = ref('');
                                     &cross;
                                 </button>
                             </div> -->
-                            </div>
                         </div>
-    
                     </div>
-                </div>
-            </div>
-        </div>
 
-        <div class="tile is-ancestor">
-            <div class="tile is-vertical">
-                <div class="tile">
-                    <!-- <div class="tile is-parent is-vertical">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora enim cumque beatae totam quis quos illum velit ipsam ullam impedit odio recusandae quo nulla quam, qui at maxime mollitia sequi?
-                        </article>
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium culpa atque repellat, aut sapiente eius suscipit inventore nobis, alias velit in laudantium tempore voluptate minus vitae? Totam at tempore ea!
-                        </article>
-                    </div> -->
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quidem. Tempora aut explicabo dolorum architecto labore delectus sit repellendus fuga soluta exercitationem dicta nisi cumque tenetur, rerum saepe cupiditate dolorem?
-                        </article>
-                    </div>
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem a, asperiores adipisci aperiam iusto, exercitationem dolor numquam quis provident corporis suscipit incidunt deleniti eum debitis doloribus quia atque aliquam dolore.
-                        </article>
-                    </div>
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores sit distinctio tenetur animi ipsam maxime ut. Quisquam ex vero, voluptatum tempora blanditiis aut quas sit nemo iusto eum maxime libero.
-                        </article>
-                    </div>
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores sit distinctio tenetur animi ipsam maxime ut. Quisquam ex vero, voluptatum tempora blanditiis aut quas sit nemo iusto eum maxime libero.
-                        </article>
-                    </div>
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores sit distinctio tenetur animi ipsam maxime ut. Quisquam ex vero, voluptatum tempora blanditiis aut quas sit nemo iusto eum maxime libero.
-                        </article>
-                    </div>
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores sit distinctio tenetur animi ipsam maxime ut. Quisquam ex vero, voluptatum tempora blanditiis aut quas sit nemo iusto eum maxime libero.
-                        </article>
-                    </div>
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores sit distinctio tenetur animi ipsam maxime ut. Quisquam ex vero, voluptatum tempora blanditiis aut quas sit nemo iusto eum maxime libero.
-                        </article>
-                    </div>
-                    <div class="tile is-parent">
-                        <article class="tile is-child box">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores sit distinctio tenetur animi ipsam maxime ut. Quisquam ex vero, voluptatum tempora blanditiis aut quas sit nemo iusto eum maxime libero.
-                        </article>
-                    </div>
                 </div>
-                <!-- <div class="tile is-parent">
-                    <article class="tile is-child box">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati iusto ipsum explicabo vel omnis perferendis tempore quidem dicta possimus! Minima voluptatibus ad cum quasi placeat? Accusantium saepe obcaecati nihil tempore.
-                    </article>
-                </div> -->
-                
+
+                <ModalVue :modelo="producto.modelo" :descripcion="producto.descripcion" :imagen="producto.imagen">
+                </ModalVue>
+
             </div>
-            <!-- <div class="tile is-parent">
-                <article class="tile is-child box">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi suscipit, atque rerum necessitatibus alias reiciendis.
-                </article>
-            </div> -->
-            
         </div>
 
     </div>
+
+
 </template>
 
 <style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+export default {
+    data() { return { showCustomContent: false } },
+    props: {
+        modelo: String,
+        descripcion: String,
+        imagen: String,
+    },
+    created() {
+        // console.log(this.modelo)
+    }
+}
+</script> 
+
+<template>
+    <va-button @click="showCustomContent = !showCustomContent">
+        Ver más
+    </va-button>
+    <va-modal v-model="showCustomContent" no-padding>
+        <template #content="{ ok }">
+            <va-image :ratio="16 / 9" :src="imagen" />
+            <va-card-title>
+                {{ modelo }}
+            </va-card-title>
+            <va-card-content>
+                {{descripcion}}
+            </va-card-content>
+            <va-card-actions>
+                <va-button @click="ok" color="warning">Ok!</va-button>
+            </va-card-actions>
+        </template>
+    </va-modal>
+</template> 
